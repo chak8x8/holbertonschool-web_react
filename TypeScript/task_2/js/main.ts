4b1eb575fece85bbdbb1b556f27ad50f43d9700b
chak8x8 <chak8x8@gmail.com>
2025-05-28 00:25:11 +1000

    5. Advanced types Part 1

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -4,3 +4,44 @@ interface DirectorInterface {
     workDirectorTasks(): string;
 }
 
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'string') {
+        salary = Number(salary);
+    }
+
+    if (salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
